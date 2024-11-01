ba5dab708b9870622a4c64664d1ebdc38ba417f3
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-11-01 10:13:58 +0200

    added sign up form to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-function App() {
+import type { FC } from 'react'
+import { IconStarGroup } from './components/icons/icon-star-group'
+import { SignUpForm } from './components/sign-up-form'
+
+export const App: FC = () => {
   return (
-    <div>
-      hello
-    </div>
+    <main className="h-full overflow-hidden flex flex-col justify-center items-center relative">
+      <IconStarGroup className="-z-10 text-[#CFE1F4] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" />
+
+      <SignUpForm className="w-full" />
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 import './css/main.scss'
+import './css/font.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
