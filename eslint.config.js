91df93650b42ee505f4fa64d853847dc75853655
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-11-01 10:15:49 +0200

    updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,19 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   react: true,
   type: true,
+
+  rules: {
+    'ts/consistent-type-definitions': ['error', 'type'],
+
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
 })
