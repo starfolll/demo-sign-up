60c221329ef296c503d9bae270fef771cf7065dc
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-11-01 10:25:36 +0200

    added github pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   clearScreen: false,
+  base: '/demo-sign-up/',
 
   plugins: [react()],
 
@@ -11,7 +12,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         api: 'modern',
-      }
-    }
-  }
+      },
+    },
+  },
 })
